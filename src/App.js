6e446854b9db4c9cab769003b8d1d6f75f16550a
David Cermeño Moranchel <davidcermenomoranchel@Davids-MacBook-Pro-2.local>
2022-06-15 22:08:02 -0500

    actividad: switch

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FullName from "./components/FullName";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
 
   const handleClick = (increase) => {
     let newCount = count;
@@ -22,6 +23,9 @@ function App() {
   const isMultiple = count % 5 === 0 && count !== 0 ? true : false;
   const countType = isMultiple ? "Es multiplo de 5" : "No es multiplo de 5";
   const countClass = isMultiple ? "active" : "inactivo";
+
+  const squareClass = isOn ? "square-on" : "square-off";
+  const squareText = isOn ? "ON" : "OFF";
   return (
     <div className="App">
       <header className="App-header">
@@ -31,6 +35,15 @@ function App() {
           Counter: {count}, {countType}
         </p>
         <button onClick={() => handleClick(true)}>Increase</button>
+
+        <div className={`square ${squareClass}`}>{squareText}</div>
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <button onClick={() => setIsOn(!isOn)}>Interruptor</button>
+        </div>
       </header>
     </div>
   );
