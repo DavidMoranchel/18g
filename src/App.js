4deee7aabd550f8152069be3317ce959675979fa
David Cermeño Moranchel <davidcermenomoranchel@Davids-MacBook-Pro-2.local>
2022-06-15 21:21:52 -0500

    tema: conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import "./App.css";
 
@@ -8,28 +8,6 @@ import FullName from "./components/FullName";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-
-  // ARGUMENTO 1: CALLBACK
-  // ARGUMENTO 2: nada, [], [count]
-  // Caso vacío: el callback del useEffect se ejecuta cada que algo cambie (estado)
-  // Caso []: el callback del useEffect se ejecuta solo la primera vez que carga el componente
-  // Caso [state]:  el callback del useEffect se ejecuta la primera vez y cada que se actualice el estado que se incluye
-  // useEffect(() => {
-  //   console.log("ME EJECUTO LA PRIMERA VEZ QUE EL COMPONENTE SE CARGA");
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log(
-  //     "ME EJECUTO LA PRIMERA VEZ QUE EL COMPONENTE SE CARGA Y CADA VEZ QUE EL ESTADO SE ACTUALIZA"
-  //   );
-  // }, [count]);
-
-  useEffect(() => {
-    console.log(
-      "ME EJECUTO LA PRIMERA VEZ Y SIEMPRE QUE HAYA UNA ACTUALIZACIÓN DEL COMPONENTE"
-    );
-  });
 
   const handleClick = (increase) => {
     let newCount = count;
@@ -41,25 +19,18 @@ function App() {
     setCount(newCount);
   };
 
-  const handleClick2 = (increase) => {
-    let newCount = count2;
-    if (increase) {
-      newCount = newCount + 1;
-    } else {
-      newCount = newCount - 1;
-    }
-    setCount2(newCount);
-  };
-
+  const isMultiple = count % 5 === 0 && count !== 0 ? true : false;
+  const countType = isMultiple ? "Es multiplo de 5" : "No es multiplo de 5";
+  const countClass = isMultiple ? "active" : "inactivo";
   return (
     <div className="App">
       <header className="App-header">
         <Title text="Aaron"></Title>
         <FullName firstName="Luis" lastName="Vera"></FullName>
-        <p>Counter: {count}</p>
+        <p className={countClass}>
+          Counter: {count}, {countType}
+        </p>
         <button onClick={() => handleClick(true)}>Increase</button>
-        <p>Counter: {count2}</p>
-        <button onClick={() => handleClick2(true)}>Increase</button>
       </header>
     </div>
   );
