da3ead5d2ec6aba9259c6e1dc7a4076d52e7d6e2
David Cermeño Moranchel <davidcermenomoranchel@Davids-MacBook-Pro-2.local>
2022-06-17 20:25:12 -0500

    tema: buss de datos && actividad: profile cards

--- src/App.js
+++ src/App.js
@@ -1,61 +1,87 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import "./App.css";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
   const [itemActive, setItemActive] = useState(null);
 
   const isActive = (itemNumber) => itemNumber === itemActive;
 
-  console.log(itemActive);
+  const koders = [
+    {
+      firstName: "Luis",
+      lastName: "Vera",
+      age: 24,
+      gender: "m",
+      photoURL: "URL VALIDA",
+    },
+    {
+      firstName: "Nestor",
+      lastName: "Ramírez",
+      age: 40,
+      gender: "m",
+      photoURL: "URL VALIDA",
+    },
+    {
+      firstName: "David",
+      lastName: "Romero",
+      age: 28,
+      gender: "m",
+      photoURL: "URL VALIDA",
+    },
+    {
+      firstName: "Yusef",
+      lastName: "Lopéz",
+      age: 40,
+      gender: "m",
+      photoURL: "URL VALIDA",
+    },
+  ];
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }, []);
+  const animals = ["parrot", "dog", "breaver", "cat"];
+  const animalsUI = animals.map((animal, index) => (
+    <li key={index}>{animal}</li>
+  ));
+  console.log(animalsUI);
 
   return (
     <div className="App">
-      {isLoading ? (
-        "Esta cargando"
-      ) : (
-        <header className="App-header">
-          <ul>
-            <li
-              onClick={() => setItemActive(1)}
-              className={`${isActive(1) ? "active" : "inactive"}`}
-            >
-              Texto 1
-            </li>
-            <li
-              onClick={() => setItemActive(2)}
-              className={`${isActive(2) ? "active" : "inactive"}`}
-            >
-              Texto 2
-            </li>
-            <li
-              onClick={() => setItemActive(3)}
-              className={`${isActive(3) ? "active" : "inactive"}`}
-            >
-              Texto 3
-            </li>
-            <li
-              onClick={() => setItemActive(4)}
-              className={`${isActive(4) ? "active" : "inactive"}`}
-            >
-              Texto 4
-            </li>
-            <li
-              onClick={() => setItemActive(5)}
-              className={`${isActive(5) ? "active" : "inactive"}`}
-            >
-              Texto 5
-            </li>
-          </ul>
-        </header>
-      )}
+      <header className="App-header">
+        <ul>
+          <li
+            onClick={() => setItemActive(1)}
+            className={`${isActive(1) ? "active" : "inactive"}`}
+          >
+            Texto 1
+          </li>
+          <li
+            onClick={() => setItemActive(2)}
+            className={`${isActive(2) ? "active" : "inactive"}`}
+          >
+            Texto 2
+          </li>
+          <li
+            onClick={() => setItemActive(3)}
+            className={`${isActive(3) ? "active" : "inactive"}`}
+          >
+            Texto 3
+          </li>
+          <li
+            onClick={() => setItemActive(4)}
+            className={`${isActive(4) ? "active" : "inactive"}`}
+          >
+            Texto 4
+          </li>
+          <li
+            onClick={() => setItemActive(5)}
+            className={`${isActive(5) ? "active" : "inactive"}`}
+          >
+            Texto 5
+          </li>
+        </ul>
+
+        <ul>{animalsUI}</ul>
+      </header>
     </div>
   );
 }
