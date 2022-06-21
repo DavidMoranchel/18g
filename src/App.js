54f9872da4cb61295dce8a7c8081a505da38e6a6
David Cermeño Moranchel <davidcermenomoranchel@Davids-MacBook-Pro-2.local>
2022-06-20 22:06:33 -0500

    tema: Formularios

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@
 import Card from "./components/Card";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const koders = [
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
+
+  const [koders, setKoders] = useState([
     {
       firstName: "Luis",
       lastName: "Vera",
       age: 24,
-      gender: "m",
-      photoURL: "https://sgame.etsisi.upm.es/pictures/18253.jpg?1621958969/",
-    },
-    {
-      firstName: "Nestor",
-      lastName: "Ramírez",
-      age: 40,
-      gender: "m",
-      photoURL: "https://sgame.etsisi.upm.es/pictures/18253.jpg?1621958969/",
-    },
-    {
-      firstName: "David",
-      lastName: "Romero",
-      age: 28,
       gender: "m",
       photoURL: "https://sgame.etsisi.upm.es/pictures/18253.jpg?1621958969/",
     },
@@ -33,26 +26,74 @@ function App() {
       gender: "f",
       photoURL: "https://media4.giphy.com/media/5sYyfIMRcpJWNqdySh/giphy.gif",
     },
-  ];
+  ]);
 
   const kodersUI = koders.map(
-    ({ firstName, lastName, age, gender, photoURL }, index) => (
-      <Card
-        key={index}
-        photoURL={photoURL}
-        firstName={firstName}
-        lastName={lastName}
-        age={age}
-        gender={gender}
-      />
-    )
+    ({ firstName, lastName, age, gender, photoURL }, index) => {
+      console.log(firstName);
+      return (
+        <Card
+          key={index}
+          photoURL={photoURL}
+          firstName={firstName}
+          lastName={lastName}
+          age={age}
+          gender={gender}
+        />
+      );
+    }
   );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("AGREGAR KODER");
+    const newKoders = [
+      ...koders,
+      {
+        firstName,
+        lastName,
+        age,
+        gender,
+        photoURL,
+      },
+    ];
+
+    setKoders(newKoders);
+
+    setFirstName("");
+    setLastName("");
+    setAge("");
+    setGender("");
+    setPhotoURL("");
+  };
+
+  console.log(koders, "KODERS");
 
   return (
     <div className="App">
       <div className="main-container">
         <div className="container">{kodersUI}</div>
       </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+        <input
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+        <input value={age} onChange={(event) => setAge(event.target.value)} />
+        <input
+          value={gender}
+          onChange={(event) => setGender(event.target.value)}
+        />
+        <input
+          value={photoURL}
+          onChange={(event) => setPhotoURL(event.target.value)}
+        />
+        <button type="submit">Agregar Koder</button>
+      </form>
     </div>
   );
 }
