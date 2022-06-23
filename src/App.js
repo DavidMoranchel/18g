c56465f4e3d9379b6c5cc96595f024524c5b998b
David Cermeño Moranchel <davidcermenomoranchel@Davids-MacBook-Pro-2.local>
2022-06-22 21:06:09 -0500

    actividad: router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
+// Components
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Cambiar a componente */}
+        <Route path="about" element={<p>I'm in about page</p>} />
+        {/* Agregar ruta y componente de perfil */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
