0862b6706ef627edf0bf8e2235c5761bdb3ed60e
David Cermeño Moranchel <davidcermenomoranchel@Davids-MacBook-Pro-2.local>
2022-06-23 21:41:23 -0500

    actividad: fetch

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+
+// Services
+import { getCharacters } from "../../services/characters";
+
 export default function Home() {
+  const [characters, setCharacters] = useState([]);
+
+  // REQUEST A Rick and Morty API.
+  useEffect(() => {
+    const getCharactersQuery = async () => {
+      const data = await getCharacters();
+      console.log(data);
+      setCharacters(data.results);
+    };
+
+    getCharactersQuery();
+  }, []);
+
+  console.log(characters, "STATE characters");
+
   return <h1>Bienvenido</h1>;
 }
+
+// 1. Componente se Monta
+// 2. Estado se define como []
+// 3. Se renderiza
+// 4. Se ejecuta el useEffect
+// 5. Se ejecuta una petición al servidor y se espera
+// 6. Con la respuesta del servidor, actualizamos el estado
+// 7. Se renderiza el componente, pero ahora el state, tiene el valor
+// del resultado del servidor
---
+++ src/services/characters.js
@@ -0,0 +1,5 @@
+export const getCharacters = async () => {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+  return data;
+};
